b0811ff84db863ae697a4765a1a84a258865d139
ian <licitdev@gmail.com>
2022-06-05 21:50:45 +0800

    Add CORS

--- workers/directus-cf-cache-worker/index.ts
+++ workers/directus-cf-cache-worker/index.ts
@@ -1,5 +1,6 @@
 import { Router, listen } from 'worktop';
 import type { KV } from 'worktop/kv';
+import * as CORS from 'worktop/cors';
 import * as api from './api';
 
 declare global {
@@ -9,6 +10,13 @@ declare global {
 }
 
 const API = new Router();
+
+API.prepare = CORS.preflight({
+	origin: '*',
+	headers: ['Cache-Control', 'Content-Type'],
+	methods: ['HEAD', 'GET', 'POST'],
+	maxage: 600,
+});
 
 API.add('GET', '/list/:collection', api.listItems);
 API.add('GET', '/list/:collection/:key', api.listItems);
