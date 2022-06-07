854a5ff1ae90e82e9fd994cfac3a5977880f91f1
ian <licitdev@gmail.com>
2022-06-07 23:17:38 +0800

    Add note to check out the API requests

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -14,8 +14,21 @@
 			This website is created using <a href="https://vuejs.org/" target="_blank">Vue.js</a> and
 			<a href="https://vitejs.dev/" target="_blank">Vite</a>.
 		</h3>
+		<h3>
+			View requests to the Cloudflare Worker running <span>directus-cf-cache</span> by opening the network tab in your
+			browser's developer console.
+		</h3>
 
 		<br />
-		<h3>Source code can be found in <a href="https://github.com/licitdev/directus-cf-cache">GitHub</a>.</h3>
+		<h3>
+			Source code and more information can be found in
+			<a href="https://github.com/licitdev/directus-cf-cache">GitHub</a>.
+		</h3>
 	</div>
 </template>
+
+<style scoped>
+span {
+	color: purple;
+}
+</style>
